029028e1a0f17c5076fd98c3acc513f9b77b5406
Quadral1403 <moritz.tomeric@gmail.com>
2023-05-15 17:00:30 +0200

    Entity Textures
    - Add Texture to Entity
    - Adapt Boundingbox to respect Entity Textures

--- src/ts/engine/entitiy/boundingbox.ts
+++ src/ts/engine/entitiy/boundingbox.ts
@@ -2,21 +2,37 @@ import Entity from "./entity.js";
 
 class BoundingBox {
   private _entity: Entity;
-  private _width: number;
-  private _height: number;
+  private _widthExpansion: number;
+  private _heightExpansion: number;
+
+  get x(): number {
+    return this._entity.x - this._widthExpansion / 2;
+  }
+
+  get y(): number {
+    return this._entity.y - this._heightExpansion / 2;
+  }
+
+  get width(): number {
+    return this._entity.texture.width + this._widthExpansion;
+  }
+
+  get height(): number {
+    return this._entity.texture.height + this._heightExpansion;
+  }
 
   constructor(entity: Entity, width: number, height: number) {
     this._entity = entity;
-    this._width = width;
-    this._height = height;
+    this._widthExpansion = width;
+    this._heightExpansion = height;
   }
 
   isInside(x: number, y: number): boolean {
     return (
-      x >= this._entity.x &&
-      x <= this._entity.x + this._width &&
-      y >= this._entity.y &&
-      y <= this._entity.y + this._height
+      x >= this.x &&
+      x <= this.x + this.width &&
+      y >= this.y &&
+      y <= this.y + this.height
     );
   }
 }
--- src/ts/engine/entitiy/entity.ts
+++ src/ts/engine/entitiy/entity.ts
@@ -1,9 +1,11 @@
+import Texture from "../assets/texture/texture.js";
 import BoundingBox from "./boundingbox.js";
 
 class Entity {
   private _x: number;
   private _y: number;
   private _boundingBox: BoundingBox;
+  private _texture: Texture;
 
   get x(): number {
     return this._x;
@@ -17,10 +19,21 @@ class Entity {
     return this._boundingBox;
   }
 
-  constructor(x: number, y: number, width: number, height: number) {
+  get texture(): Texture {
+    return this._texture;
+  }
+
+  constructor(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    texture: Texture
+  ) {
     this._x = x;
     this._y = y;
     this._boundingBox = new BoundingBox(this, width, height);
+    this._texture = texture;
   }
 
   update(delta: number) {}
