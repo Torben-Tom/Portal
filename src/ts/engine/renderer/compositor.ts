b6df6107a1ec59ab3f4b5c427ac6c1fca1e246e0
Kitt3120 <torben@schweren.dev>
2023-05-25 19:51:55 +0200

    Compositor improvements
    Initialize the glContext in the Compositor instead of in the RenderPipeline.
    Use definitive assignment assertions to mark some of the private variables as initialized. By doing this, the TypeScript compiler no longer shows false errors about the variables not being assigned before their usage.
    Add return types to Compositor methods.

--- src/ts/engine/renderer/compositor.ts
+++ src/ts/engine/renderer/compositor.ts
@@ -4,21 +4,70 @@ import RenderPipeline from "./renderpipeline.js";
 import RenderPipelineBuilder from "./renderpipelinebuilder.js";
 
 class Compositor {
-  private _renderPipeline: RenderPipeline;
+  private _htmlCanvasElement: HTMLCanvasElement;
+  private _canvasWidth!: number;
+  private _canvasHeight!: number;
+  private _glContext!: CanvasRenderingContext2D;
+  private _renderPipeline!: RenderPipeline;
   private _entityManager: EntityManager;
+
+  get canvasWidth(): number {
+    return this._canvasWidth;
+  }
+
+  get canvasHeight(): number {
+    return this._canvasHeight;
+  }
+
+  set canvasWidth(width: number) {
+    this._htmlCanvasElement.width = width;
+    this._canvasWidth = width;
+  }
+
+  set canvasHeight(height: number) {
+    this._htmlCanvasElement.height = height;
+    this._canvasHeight = height;
+  }
 
   constructor(
     htmlCanvasElement: HTMLCanvasElement,
     entityManager: EntityManager
   ) {
-    this._renderPipeline = new RenderPipelineBuilder(htmlCanvasElement)
-      .useRenderer(new EntityRenderer())
-      .build();
-
+    this._htmlCanvasElement = htmlCanvasElement;
+    this.refreshSize();
+    this.initializeGlContext();
+    this.initializeRenderPipeline();
     this._entityManager = entityManager;
   }
 
-  render(delta: number) {
+  private initializeGlContext(): void {
+    let glContext = this._htmlCanvasElement.getContext("2d");
+    if (!glContext) {
+      throw new Error("Could not get 2D context");
+    }
+    this._glContext = glContext;
+    this._glContext.imageSmoothingEnabled = false;
+  }
+
+  private initializeRenderPipeline(): void {
+    this._renderPipeline = new RenderPipelineBuilder(this._glContext)
+      .useRenderer(new EntityRenderer())
+      .build();
+  }
+
+  private refreshSize(): void {
+    this._canvasWidth = this._htmlCanvasElement.width;
+    this._canvasHeight = this._htmlCanvasElement.height;
+  }
+
+  private clearCanvas(): void {
+    this._glContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+  }
+
+  render(delta: number): void {
+    this.refreshSize();
+    this.clearCanvas();
+
     this._entityManager.entities.forEach((entity) => {
       this._renderPipeline.render(entity, delta);
     });
--- src/ts/engine/renderer/renderpipelinebuilder.ts
+++ src/ts/engine/renderer/renderpipelinebuilder.ts
@@ -2,19 +2,19 @@ import Renderer from "./renderer.js";
 import RenderPipeline from "./renderpipeline.js";
 
 class RenderPipelineBuilder {
-  protected _htmlCanvasElement: HTMLCanvasElement;
+  protected _glContext: CanvasRenderingContext2D;
   protected _renderers: Renderer[];
 
-  get htmlCanvasElement(): HTMLCanvasElement {
-    return this._htmlCanvasElement;
+  get glContext(): CanvasRenderingContext2D {
+    return this._glContext;
   }
 
   get renderers(): Renderer[] {
     return this._renderers;
   }
 
-  constructor(htmlCanvasElement: HTMLCanvasElement) {
-    this._htmlCanvasElement = htmlCanvasElement;
+  constructor(glContext: CanvasRenderingContext2D) {
+    this._glContext = glContext;
     this._renderers = [];
   }
 
